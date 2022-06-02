a5f5c2818753e8ded9dd2f8779ead9492f204941
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2022-06-02 15:42:43 +0430

    Add button for enter to translate

--- 2022-1/2- translator with python/1-first try.py
+++ 2022-1/2- translator with python/1-first try.py
@@ -5,18 +5,34 @@ import tkinter as tk
 from deep_translator import GoogleTranslator
 
 window_translator = tk.Tk()
-window_translator.title('Sina Lalebakhsh')
+window_translator.title('Sina Lalebakhsh || سینا لاله بخش || Translator')
 
 help_text = tk.Label(
     master=window_translator,
     text='Write your text - متن خودتو بنویس',
     width=30,
-    height=3,
+    height=2,
     font=('Arial', '30'),
 )
 help_text.grid(row=0, column=0, columnspan=3)
 
+user_text = tk.Entry(
+    master=window_translator,
+    text='Write your text - متن خودتو بنویس',
+    width=30,
+    font=('Arial', '20'),
+)
+user_text.grid(row=1, column=0, columnspan=3)
 
+button_to_translate = tk.Button(
+    master=window_translator,
+    text='Enter to translate',
+    width=15,
+    bg='#f4004f',
+    height=2,
+    font=('Arial', '15'),
+)
+button_to_translate.grid(row=2, column=0, columnspan=3)
 
 window_translator.mainloop()
 
