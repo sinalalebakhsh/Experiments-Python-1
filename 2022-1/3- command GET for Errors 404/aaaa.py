80c44a61d5f502b0e92ce31bb9df254721e76dda
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2022-06-08 00:57:18 +0430

    Setting another change for program: diagnosis 200 or 404 status code

--- 2022-1/3- command GET for Errors 404/aaaa.py
+++ 2022-1/3- command GET for Errors 404/aaaa.py
@@ -4,7 +4,8 @@ http1 = requests.get('https://www.salamatiyeshoma.com')
 http2 = requests.get('https://salamatiyeshoma.com')
 http3 = requests.get('https://google.com')
 
-print(http1)
-print(http2)
-print(http3)
+print(http1.content)
+print(http2.status_code)
+# print(http3.content)
+print(http3.status_code)
 
