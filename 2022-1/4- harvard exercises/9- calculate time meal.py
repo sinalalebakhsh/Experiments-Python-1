4711f7ca1c32859a63177f1f8ba7cd7548c1646b
Sina Lalebakhsh <93151188+sinalalebakhsh@users.noreply.github.com>
2022-06-29 00:56:28 +0430

    this is another try
    to create clean code

--- 2022-1/4- harvard exercises/9- calculate time meal.py
+++ 2022-1/4- harvard exercises/9- calculate time meal.py
@@ -1,3 +1,17 @@
-a = '7:30'
-a = a.split(':')
-print(a)
+
+def main():
+    orginal_time = input('Enter time: ')
+    converted_time = convert(orginal_time)
+    if converted_time >= 7.0 and converted_time <= 8.0 :
+        print('breakfast time')
+    else:
+        return
+
+
+def convert(orginal_number):
+    hours , minutes = orginal_number.split(':')
+    hours = float(hours)
+    minutes = float(minutes) * 60 / 3600
+    return hours + minutes
+
+main()
