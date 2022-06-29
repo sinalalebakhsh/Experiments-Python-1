9170eb33115ab2bc3f3143122c45c69b3113b83e
Sina Lalebakhsh <93151188+sinalalebakhsh@users.noreply.github.com>
2022-06-30 00:14:53 +0430

    it is last version
    in this program i try ti fix every condition
    if you have another, please help me.
    thanks

--- 2022-1/4- harvard exercises/9- calculate time meal.py
+++ 2022-1/4- harvard exercises/9- calculate time meal.py
@@ -1,9 +1,12 @@
-
 def main():
     orginal_time = input('Enter time: ')
     converted_time = convert(orginal_time)
     if converted_time >= 7.0 and converted_time <= 8.0 :
         print('breakfast time')
+    elif converted_time >= 12.0 and converted_time <= 13.0 :
+        print('lunch time')
+    elif converted_time >= 18.0 and converted_time <= 19.0 :
+        print('dinner time')
     else:
         return
 
